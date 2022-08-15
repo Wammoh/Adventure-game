ae319716942721cd3584baabf06de54ef9a8de38
TroyJohnson <53130184+Wammoh@users.noreply.github.com>
2022-08-14 22:24:53 -0500

    Main files
    Main file

---
+++ adventure_gamever1.2.py
@@ -0,0 +1,6 @@
+ 
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+
+console.log('The Intro to JavaScript course is ' + adjective1'. James and Julia are so ' + adjective2 + '. I cannot wait to work through the rest of this ' + adjective3' content!');
